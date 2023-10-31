4bd7c7d941792a8e974b89cdf5fe6b4184c4b859
xuluw <xw370@georgetown.edu>
2023-10-31 13:42:29 -0400

    modified app.py

--- Project/app.py
+++ Project/app.py
@@ -2,81 +2,97 @@ import streamlit as st
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
-import os
 import pandas as pd
 import plotly.express as px
 
 # Read data
-df = pd.read_csv("datasets/downsampled_smoking_driking_dataset.csv")
+df = pd.read_csv("datasets/downsampled_dataset_after_feature_selection.csv")
 # Load saved model from pickle file
 with open("saved_stacked_models/StackedPickleDrinking.pkl", 'rb') as file:
     pickle_drink_model = pickle.load(file)
 with open("saved_stacked_models/StackedPickleSmoking.pkl", 'rb') as file:
     pickle_smoke_model = pickle.load(file)
 
+# Streamlit Setup
+# set title
+st.title('Smoking and Drinking Status Prediction App')
+# set description
+st.markdown('## Boxplots for Important Features with Drinking and Smoking Status')
+
+
 # Important features
 important_features_drk = ["gamma_GTP","age"]
 important_features_smk = ["age","hemoglobin"]
 
 # Boxplot for each target variable
-for i in important_features_drk:
-    plot_drk = px.box(df, x=df[i], y=df["DRK_YN"])
-    plot_drk.update_layout(
-    title=f'Box Plot of {i} vs {df["DRK_YN"]}',
-    xaxis_title=i,
-    yaxis_title="Drinking Status"
-    )
+drk_gamma = px.box(df, y=df["gamma_GTP"], x=df["DRK_YN"])
+drk_gamma.update_layout(
+    title=f'Box Plot of gamma_GTP vs drinking status',
+    yaxis_title="gamma_GTP",
+    xaxis_title="Drinking Status")
 
-for i in important_features_smk:
-    plot_drk = px.box(df, x=df[i], y=df["SMK_stat_type_cd"])
-    plot_drk.update_layout(
-    title=f'Box Plot of {i} vs {df["SMK_stat_type_cd"]}',
-    xaxis_title=i,
-    yaxis_title="Smoking Status"
-    )
+drk_age = px.box(df, y=df["age"], x=df["DRK_YN"])
+drk_age.update_layout(
+    title=f'Box Plot of age vs drinking status',
+    yaxis_title="age",
+    xaxis_title="Drinking Status")
 
-# Streamlit Setup
-# set title
-st.title('Smoking and Drinking Status Prediction App')
-# set description
-st.write('Boxplots for Important Features with Drinking and Smoking Status')
-# add header 2 through markdown
-# st.markdown("## Smoking Status:")
+smk_age = px.box(df, y=df["age"], x=df["SMK_stat_type_cd"])
+smk_age.update_layout(
+    title=f'Box Plot of age vs smoking status',
+    yaxis_title="age",
+    xaxis_title="Smoking Status")
 
-col1, col2 = st.columns(2)
+smk_hemog = px.box(df, y=df["hemoglobin"], x=df["SMK_stat_type_cd"])
+smk_hemog.update_layout(
+    title=f'Box Plot of hemoglobin vs smoking status',
+    yaxis_title="hemoglobin",
+    xaxis_title="Smoking Status")
 
-with col1:
-   st.header("Important Feature with Drinking Status")
-#    st.image("https://static.streamlit.io/examples/cat.jpg")
-   st.plotly_chart(plot_drk)
 
-with col2:
-   st.header("Important Feature with Smoking Status")
-#    st.image("https://static.streamlit.io/examples/dog.jpg")
-   st.plotly_chart(plot_drk)
+# Two tabs
+tab1, tab2 = st.tabs(["Drinking Status", "Smoking Status"])
+
+with tab1:
+    st.text("Drink Status: 0 (No), 1(Yes)")
+    st.plotly_chart(drk_gamma)
+    st.plotly_chart(drk_age)
+
+with tab2:
+    st.text("Smoke Status: 0 (Never), 1 (Used to smoke but quit), 2 (Still smoke)")
+    st.plotly_chart(smk_age)
+    st.plotly_chart(smk_hemog)
+
 
 # add header 2 through markdown
 st.markdown("## Target Variable Prediction")
 
-st.markdown('### Input Parameters')
+st.markdown('### Data Overview')
+st.dataframe(df.head(2))
 
-feature_vec = st.text_input("Input a comma-seperated list of features (20): ")
-st.markdown('Example: List of features: 0,35,81,0.5,0.6,1,1,93,53,85,69,117,30,11.3,1,0.8,20,7,10,23.4')
+st.markdown('### Input Parameters')
+feature_vec = st.text_input("Input a comma-seperated list of features (20): ", "0,35,81,0.5,0.6,1,1,93,53,85,69,117,30,11.3,1,0.8,20,7,10,23.4")
+st.markdown('Example:')
+st.markdown('0,50,72.0,1.5,1.5,1.0,1.0,133.0,89.0,93.0,56.0,157.0,72.0,14.4,1.0,0.8,20.0,21.0,20.0,18.7')
+st.markdown('0,45,84,1.2,1.2,1,1,121,80,102,43,133,274,13.4,1,0.7,14,11,16,23.4')
+st.markdown('1,40,105.0,1.2,1.2,1.0,1.0,126.0,69.0,125.0,57.0,92.0,83.0,16.4,1.0,1.0,38.0,33.0,21.0,27.8')
+st.markdown('0,30,68.5,1.0,1.0,1.0,1.0,124.0,82.0,89.0,63.0,132.0,110.0,14.4,1.0,0.9,21.0,14.0,12.0,19.5')
+st.markdown('1,65,96.0,1.2,1.2,1.0,1.0,144.0,82.0,167.0,49.0,39.0,167.0,14.7,1.0,1.7,17.0,12.0,81.0,29.3')
+
 
 x = feature_vec.split(",")
-x = [float(i) for i in x] 
+x = [float(i) for i in x]
 x = np.array(x).reshape(1, -1)
 
 
-st.markdown('### Drinking Status Prediction')
 y_drink_predict = pickle_drink_model.predict(x)[0]
 if y_drink_predict==0:
     drink_res='Drinker'
 elif y_drink_predict==1:
     drink_res='Non-drinker'
-st.write(drink_res)
+st.markdown("#### Drinking Status Prediction: "+drink_res)
 
-st.markdown('### Smoking Status Prediction')
+
 y_smoke_predict = pickle_smoke_model.predict(x)[0]
 if y_smoke_predict==0:
     smoke_res='Never Smoking'
@@ -84,4 +100,4 @@ elif y_smoke_predict==1:
     smoke_res='Used to smoke but quit'
 elif y_smoke_predict==2:
     smoke_res="Still smoking"
-st.write(smoke_res)
+st.markdown("#### Smoking Status Prediction: "+smoke_res)
